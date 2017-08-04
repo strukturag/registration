4a6d0ced14b4c89fe70f4ff9fee12de3d6afd923
Leon Klingele <leon@struktur.de>
2017-08-04 14:22:02 +0200

    Also include lowercase characters in generated tokens

--- db/pendingregist.php
+++ db/pendingregist.php
@@ -22,7 +22,7 @@ class PendingRegist {
 		$query = $this->db->prepare('INSERT INTO `*PREFIX*registration`'
 			. ' ( `email`, `token`, `requested` ) VALUES( ?, ?, NOW() )');
 
-		$token = $this->random->generate($this->tokenLength, ISecureRandom::CHAR_UPPER . ISecureRandom::CHAR_DIGITS);
+		$token = $this->random->generate($this->tokenLength, ISecureRandom::CHAR_UPPER . ISecureRandom::CHAR_LOWER . ISecureRandom::CHAR_DIGITS);
 
 		$query->execute(array($email, TokenHasher::hash($token)));
 		return $token;
